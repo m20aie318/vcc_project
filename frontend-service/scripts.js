8c128236b4fe2a109d588b721e06ea19eec87241
m20aie318 <thusoo.1@iitj.ac.in>
2023-11-25 18:12:59 +0530

    UI fix

--- frontend-service/scripts.js
+++ frontend-service/scripts.js
@@ -32,23 +32,7 @@ async function fetchNotes() {
           <p class="card-text"> ${note.title}</p>
           <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#${modelid}">Details</button>
         </div>
-      </div>
-      <div id="${modelid}" class="modal-dialog fade" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title">${note.title}</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-                ${note.content}
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
-          </div>
-        </div>`;
+      </div>`;
       size = size + 1
     });
 
